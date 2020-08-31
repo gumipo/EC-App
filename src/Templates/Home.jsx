f8caa02806e0da77d50f542b2e344775d9202336
gumipo <10sei0otto.22@gmail.com>
2020-08-31 16:13:22 +0900

    create usersSelector storeからのstate取得関数

--- src/Templates/Home.jsx
+++ src/Templates/Home.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { push } from "connected-react-router";
+import { getUserId, getUserName } from "../reducks/Users/selector";
 
 const Home = () => {
-  const dispatch = useDispatch();
   const selector = useSelector((state) => state);
-  console.log(selector.router);
+  const uid = getUserId(selector);
+  const username = getUserName(selector);
+
+  const dispatch = useDispatch();
+
   return (
     <div>
       <h2>Home</h2>
+      <p>{uid}</p>
+      <p>{username}</p>
 
       <button onClick={() => dispatch(push("/login"))}>ログアウト</button>
     </div>
